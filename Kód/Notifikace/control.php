c10bf3344d48f126ca271ae5bdd82855da86a143
ZidanMotorko9 <147751571+ZidanMotorko9@users.noreply.github.com>
2023-12-06 20:15:27 +0100

    Add files via upload

---
+++ Kód/Notifikace/control.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rsp_casopis";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT status FROM clanek WHERE autor = :autor");
+    $stmt->bindParam(':autor', $autor_value);
+    // $stmt->bindParam(':ukazatel', $ukazatel_value);
+
+    // Nastavte hodnotu autora a dalšího ukazatele podle potřeby
+    $autor_value = "jenda";
+    // $ukazatel_value = 1;
+
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        echo "<p>Status: " . htmlspecialchars($row["status"]) . "</p>";
+    } else {
+        echo "Nenalezen žádný článek s daným autorem a ukazatelem.";
+    }
+} catch (PDOException $e) {
+    echo "Chyba při práci s databází: " . $e->getMessage();
+}
+
+$conn = null;
+?>
+
+
